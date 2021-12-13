57d3d8cf0c7352b4ea0fe475c409d7c922e32222
chasanalim <chasanalim07@example.com>
2021-12-13 08:48:23 +0700

    Penambahan double klik untuk cetak label RM

---
+++ src/rekammedis/PanelRiwayat.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rekammedis;
+
+/**
+ *
+ * @author asus
+ */
+public class PanelRiwayat {
+
+    public void setBorder(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setName(String panelResume1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
